8a0f17666a82253c48aec4c54c01d87120bfa679
Guan <guan@DESKTOP-F2PL8ID.localdomain>
2020-12-04 00:41:20 +0800

    finish post and get

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import RoomView
+from .views import RoomView, CreateRoomView, GetRoom
 
 urlpatterns = [
-    path('room', RoomView.as_view()),
+    path('room/list', RoomView.as_view()),
+    path('room/create', CreateRoomView.as_view()),
+    path('room/get',  GetRoom.as_view()),
 ]
